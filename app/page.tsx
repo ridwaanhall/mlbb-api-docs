1df2287976b4b4c5de109aebe340a58ec5b48dbe
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-07 14:23:27 +0700

    add error handling, layout, and not found pages for improved user experience

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import { buttonVariants } from "@/components/ui/button";
+import { page_routes } from "@/lib/routes-config";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
+      <h1 className="text-5xl font-bold mb-4 sm:text-7xl">
+        Docs of <span className="text-primary">MLBB API</span>{" "}
+      </h1>
+      <h1 className="text-3xl font-bold mb-4 sm:text-5xl">
+        An API for Mobile Legends: Bang Bang
+      </h1>
+      <p className="mb-8 sm:text-md max-w-[800px] text-muted-foreground">
+      This API documentation provides comprehensive data for Mobile Legends: Bang Bang. Including statistics, hero information, and other relevant details. Example usage of this API documentation is provided in Python.
+      </p>
+      <div>
+        <Link
+          href={`/docs${page_routes[0].href}`}
+          className={buttonVariants({
+            className: "px-6 !font-medium",
+            size: "lg",
+          })}
+        >
+          Get Stared
+        </Link>
+      </div>
+    </div>
+  );
+}
