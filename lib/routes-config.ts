70989b8da7eb39d6b4f48faf0785d52cc882f873
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-07 15:27:56 +0700

    add GET Hero Rate endpoint and documentation

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -26,6 +26,7 @@ export const ROUTES: EachRoute[] = [
       { title: "GET Hero Detail", href: "/get-hero-detail" },
       { title: "GET Hero Detail Stats", href: "/get-hero-detail-stats" },
       { title: "GET Hero Skill Combo", href: "/get-hero-skill-combo" },
+      { title: "GET Hero Rate", href: "/get-hero-rate" },
     ],
   },
 ];
